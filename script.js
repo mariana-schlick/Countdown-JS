0e710c09a9cd812bfaf518521fe2567aefdd3871
Mariana Schlickmann | Xu <157842385+mariana-schlick@users.noreply.github.com>
2024-02-12 16:22:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const mensagem = "Bom te ver aqui! "
+alert(mensagem + "AAAH!")
+
+const daysElement = document.querySelector("[data-days]");
+const hoursElement = document.querySelector("[data-hours]");
+const minutesElement = document.querySelector("[data-minutes]");
+const secondsElement = document.querySelector("[data-seconds]");
+
+const render = (days, hours, minutes, seconds) => {
+    daysElement.innerHTML = String(days).padStart("2", 0);
+    hoursElement.innerHTML = String(hours).padStart("2", 0);
+    minutesElement.innerHTML = String(minutes).padStart("2", 0);
+    secondsElement.innerHTML = String(seconds).padStart("2", 0);
+};
+
+const countdown = ()=>{
+const now = new Date();
+const targetDate = new Date(2024, 9, 5);
+
+const timeLeft = targetDate - now;
+
+const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((timeLeft % (1000 * 60)) / (1000));
+
+render(days, minutes, hours, seconds)
+
+};
+
+setInterval(countdown, 1000);
